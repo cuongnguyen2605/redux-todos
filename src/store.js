eb204cca96ef4db4b1e0fc836c5391a736c46b80
Cuong Nguyen <cuongnmd00335@gmail.com>
2018-07-17 01:32:00 +0700

    remove redux thunk

--- src/store.js
+++ src/store.js
@@ -1,16 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk                                     from 'redux-thunk';
 import rootReducer                               from './reducers';
-
-const initialState = {};
-
-const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
   compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
